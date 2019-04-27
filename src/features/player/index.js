3b01e12b339875f987921f83ef4df210b7bfc955
fatihturgut <ftfatihft@gmail.com>
2019-04-27 23:27:34 +0300

    prettier

--- src/features/player/index.js
+++ src/features/player/index.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import walkSprite from './assets/player-walk.png';
-import handleMovement from './movement';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import walkSprite from "./assets/player-walk.png";
+import handleMovement from "./movement";
 
 class Player extends Component {
   render() {
     const { position } = this.props;
     return (
-      <div 
+      <div
         style={{
-          position: 'absolute',
+          position: "absolute",
           top: position[1],
           left: position[0],
           backgroundImage: `url('${walkSprite}')`,
-          backgroundPosition: '0 0',
-          width: '40px',
-          height: '40px',
-        }} 
+          backgroundPosition: "0 0",
+          width: "40px",
+          height: "40px"
+        }}
       />
     );
   }
 }
 
 const mapStateToProps = state => ({
-  ...state.player,
+  ...state.player
 });
 
 export default connect(mapStateToProps)(handleMovement(Player));
